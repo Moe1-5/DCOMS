a755d150a8586271097f01b968c987fb2607b3bf
naawkaz <ahmedzakwaanshareef@gmail.com>
2026-04-10 15:08:39 +0100

    feat: implement async service call

--- src/main/java/finalproject/com/dcoms/client/controller/EmployeeController.java
+++ src/main/java/finalproject/com/dcoms/client/controller/EmployeeController.java
@@ -7,27 +7,40 @@ package finalproject.com.dcoms.client.controller;
 import finalproject.com.dcoms.client.view.EmployeeDashboard;
 import finalproject.com.dcoms.remote.HRMService;
 import java.rmi.RemoteException;
+import javax.swing.SwingWorker;
 
 /**
  *
  * @author zaku
  */
 public class EmployeeController {
+
     private HRMService service;
     private EmployeeDashboard employeeDashboard;
-    
+
     public EmployeeController(HRMService service, EmployeeDashboard employeeDashboard) {
         this.service = service;
         this.employeeDashboard = employeeDashboard;
     }
-    
-    public void loadName(String employeeId){
-        try {
-            String[] employeeDetails = service.getEmployeeById("1");
-            String name = employeeDetails[1];
-            employeeDashboard.setTitle(name);
-        } catch (RemoteException e) {
-            System.out.println("Failed to load employee: " + e.getMessage());
-        }
+
+    public void loadName(String employeeId) {
+        // SwingWorker enables async calls to server separate from ui thread
+        new SwingWorker<String[], Void>() {
+            @Override
+            protected String[] doInBackground() throws Exception {
+                return service.getEmployeeById("1");
+            }
+            
+            @Override
+            protected void done() {
+                try {
+                    String[] employeeDetails = get();
+                    String name = employeeDetails[1];
+                    employeeDashboard.setTitle(name);
+                } catch (Exception e) {
+                    System.out.println("Failed to load employee: " + e.getMessage());
+                }
+            }
+        }.execute();
     }
 }
