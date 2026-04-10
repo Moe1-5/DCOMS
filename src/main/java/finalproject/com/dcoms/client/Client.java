7a2496e176892371ad9c3bab2392d8bd926ffdb1
naawkaz <ahmedzakwaanshareef@gmail.com>
2026-04-11 00:17:26 +0800

    Create directories

---
+++ src/main/java/finalproject/com/dcoms/client/Client.java
@@ -0,0 +1,12 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package finalproject.com.dcoms.client;
+/**
+ *
+ * @author zaku
+ */
+public class Client {
+    
+}
